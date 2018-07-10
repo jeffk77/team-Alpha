d434c9a4bce9daf05d3d6bb46afe05de9674d1d5
Kwong <aykwong93@gmail.com>
2018-07-10 19:22:47 -0400

    Google maps api marker placement working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,27 @@
 var places = {
-    toronto: {lat: 43.653908, lng: -79.384293},
-    oise: {lat: 43.668117, lng: -79.398363},
-    eatonCenter: {lat: 43.654828, lng: -79.380703},
-    harbourfrontCenter: {lat: 43.638927, lng: -79.381906}
+    toronto: { lat: 43.653908, lng: -79.384293 },
+    oise: { lat: 43.668117, lng: -79.398363 },
+    eatonCenter: { lat: 43.654828, lng: -79.380703 },
+    harbourfrontCenter: { lat: 43.638927, lng: -79.381906 }
 };
 
 var map;
 
-// var location = {lat: 43.653908, lng: -79.384293};
+var lcn = { lat: 43.653908, lng: -79.384293 };
 
-$(".btn").on("click", function places() {
-    let location = $(this).val();
-    console.log(location);
+$(".btn").on("click", function () {
+    lcn = places[$(this).val()];
+    console.log(lcn);
+    console.log(places[lcn]);
     initMap();
 });
 
-
 //Google Maps
 function initMap() {
+    console.log("inside " + JSON.stringify(lcn));
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 43.653908, lng: -79.384293},
-        zoom: 13
+        center: lcn,
+        zoom: 14
     });
-    // let marker = new google.maps.Marker({position: location, map: map})
+    let marker = new google.maps.Marker({ position: lcn, map: map })
 };
